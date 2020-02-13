9407d9150cdff3eee4cd8b7412b9b50c79db0cbd
seanaleid <seanaleid@gmail.com>
2020-02-12 21:51:18 -0500

    Initializes repo for practice
      * Adds starter code, server, client directory and basic CRA files

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 5000;
+const app = express();
+const { data } = require('./data');
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const footballers = data.map((footballer, index) => ({ ...footballer, id: index }));
+
+app.get('/api/footballers', (req, res) => {
+    res.send(footballers);
+});
+
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+});
